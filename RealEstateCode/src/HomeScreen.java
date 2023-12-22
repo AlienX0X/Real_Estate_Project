8a47e67adb7178bc64cf97f1786bbfa7680e9512
AlienX0X <ahmedeltras10@gmail.com>
2023-12-22 05:48:40 -0800

    the new image in home Screen

--- RealEstateCode/src/HomeScreen.java
+++ RealEstateCode/src/HomeScreen.java
@@ -1,8 +1,14 @@
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.net.URL;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 class HomeScreen {
 
@@ -13,18 +19,48 @@ class HomeScreen {
     public HomeScreen(Runnable onGoToSearchButtonClick) {
         this.onGoToSearchButtonClick = onGoToSearchButtonClick;
 
-        panel = new JPanel(new GridLayout(2, 1));
+        panel = new JPanel(new BorderLayout());
 
-        JLabel lblWelcome = new JLabel("Welcome to Real Estate Project!");
+        JLabel lblWelcome = new JLabel("Welcome to Real Estate Project!", SwingConstants.CENTER);
+        lblWelcome.setFont(new Font("Arial", Font.BOLD, 20));
+        
+        // Add an image to the panel
+        ImageIcon imageIcon = createImageIcon("pexels-binyamin-mellish-186077.jpg"); // Replace with the actual image file
+        if (imageIcon != null) {
+            JLabel imageLabel = new JLabel(imageIcon);
+            panel.add(imageLabel, BorderLayout.CENTER);
+        }
+
+        // Add details text to the panel
+        JTextArea detailsTextArea = new JTextArea();
+        detailsTextArea.setEditable(false);
+        detailsTextArea.setLineWrap(true);
+        detailsTextArea.setWrapStyleWord(true);
+        detailsTextArea.setText("Explore our real estate listings and find your dream home!\n\n"
+                + "Our platform offers a wide range of properties in various locations.\n"
+                + "Start your search now!");
+
+        // Add a button to navigate to the search screen
         goToSearchButton = new JButton("Go to Search");
-
         goToSearchButton.addActionListener(e -> onGoToSearchButtonClick.run());
 
-        panel.add(lblWelcome);
-        panel.add(goToSearchButton);
+        // Add components to the panel
+        panel.add(lblWelcome, BorderLayout.NORTH);
+        panel.add(detailsTextArea, BorderLayout.SOUTH);
+        panel.add(goToSearchButton, BorderLayout.PAGE_END);
     }
 
     public JPanel getPanel() {
         return panel;
     }
-}
+
+    // Method to create an image icon from the file name
+    private ImageIcon createImageIcon(String fileName) {
+        URL imgUrl = getClass().getResource(fileName);
+        if (imgUrl != null) {
+            return new ImageIcon(imgUrl);
+        } else {
+            System.err.println("Couldn't find file: " + fileName);
+            return null;
+        }
+    }}
--- RealEstateCode/src/RealEstateProjectGUI.java
+++ RealEstateCode/src/RealEstateProjectGUI.java
@@ -20,6 +20,29 @@ public class RealEstateProjectGUI {
 
     public RealEstateProjectGUI() {
         frame = new JFrame("Real Estate Project");
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        mainPanel = new JPanel(new CardLayout());
+
+        loginScreen = new LoginScreen(this::showRegisterScreen, this::showHomeScreen);
+        registerScreen = new RegisterScreen(this::showLoginScreen);
+        homeScreen = new HomeScreen(this::showSearchScreen);
+        searchScreen = new SearchScreen();
+
+        mainPanel.add(loginScreen.getPanel(), "Login");
+        mainPanel.add(registerScreen.getPanel(), "Register");
+        mainPanel.add(homeScreen.getPanel(), "Home");
+        mainPanel.add(searchScreen.getPanel(), "Search");
+
+        CardLayout cardLayout = (CardLayout) mainPanel.getLayout();
+        cardLayout.show(mainPanel, "Login");
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
+    } {
+        frame = new JFrame("Real Estate Project");
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
